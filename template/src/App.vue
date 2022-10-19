17a9e78fca2208b1c50a41202aa8ac7d82b674ac
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-10-18 21:58:52 -0500

    Added dummy default example

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 
+const props = defineProps({
+    text: {
+        type: String,
+        default: "Hello world!"
+    }
+});
+
 </script>
 
 <template>
+
+<p>{{ props.text }}</p>
 
 </template>
