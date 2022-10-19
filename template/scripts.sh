b398137e620558b483b880283d16a2d168e65123
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-10-18 21:30:40 -0500

    Corrected bugs of the scripts.sh file

--- template/scripts.sh
+++ template/scripts.sh
@@ -9,12 +9,12 @@ function remove() {
     |tailwind.config.js
     |vite.config.js
     |scripts.sh
-    |" >> ./.gitignore
+    |" >> ./.gitignore;
 }
 
 function unstub() {
-    for directory in executable images do
-        rm "./$directory/.stub"
+    for directory in executable images; do
+        rm "./$directory/.stub";
     done
 }
 
@@ -26,12 +26,20 @@ function gitalize() {
 
 function go-vue() {
     install;
+    echo;
     echo "Finished installing node modules"
+    echo;
     remove;
+    echo;
     echo "Finished removing unnecessary files from the project (they were added to the .gitignore)"
+    echo;
     unstub;
+    echo;
     echo "Finished removing stub from empty directories"
+    echo;
     gitalize;
+    echo;
     echo "Finished creating git project"
-    echo "Now run `npm run tail` and `npm run dev` in different shells to start development"
+    echo "Now run \`npm run tail\` and \`npm run dev\` in different shells to start development"
+    echo;
 }
