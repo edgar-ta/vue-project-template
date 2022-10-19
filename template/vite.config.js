2b980df9cb12c9eb479703819e820cb8fbce15b2
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-10-18 21:51:00 -0500

    Changed entry point in vite.config.js

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,9 +1,20 @@
+import { resolve } from "path";
+
 /** @type {import('vite').UserConfig} */
 export default {
     build: {
         // make ./executable the output directory, as explained in 
         // the link below: 
         // https://vitejs.dev/config/build-options.html#build-outdir
-        outDir: "executable"
+        outDir: "executable",
+        rollupOptions: {
+            input: {
+                // this configuration is intended to possibilitate
+                // multi page apps, but i use it to change the
+                // entry point of mine:
+                // https://vitejs.dev/guide/build.html#multi-page-app
+                main: resolve(__dirname, "main.html")
+            }
+        }
     }
 };
