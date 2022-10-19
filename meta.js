e25ac2900b73d7078612b0311a6f6ac16b3a13bc
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-10-18 19:19:47 -0500

    Basic Structure
    Added the basic structure to the template
    (there doesn't seem to be missing anything)

---
+++ meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+    prompts: {
+        projectName: {
+            type: "string",
+            required: true,
+            message: "Ingresa el nombre del proyecto (en kebab-case)"
+        }
+    },
+    completeMessage: "¡Proyecto creado!"
+};
+
---
+++ template/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './src/App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
