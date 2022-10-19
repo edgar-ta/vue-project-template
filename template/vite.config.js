9dd319601fe1a7887eb788dfc7fb9c4b10bc86dd
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-10-18 22:13:47 -0500

    Renamed files

--- template/vite.config.js
+++ template/vite.config.js
@@ -7,14 +7,5 @@ export default {
         // the link below: 
         // https://vitejs.dev/config/build-options.html#build-outdir
         outDir: "executable",
-        rollupOptions: {
-            input: {
-                // this configuration is intended to possibilitate
-                // multi page apps, but i use it to change the
-                // entry point of mine:
-                // https://vitejs.dev/guide/build.html#multi-page-app
-                main: resolve(__dirname, "main.html")
-            }
-        }
     }
 };
